44de5eded71baf400468f325941366442d529245
FauxKiwi <sfn14ok04@gmail.com>
2021-06-15 21:47:26 +0200

    Added plus and minus for Vectors

--- src/main/kotlin/com/wynnlab/extensions/operators.kt
+++ src/main/kotlin/com/wynnlab/extensions/operators.kt
@@ -3,6 +3,10 @@ package com.wynnlab.extensions
 import org.bukkit.Location
 import org.bukkit.util.Vector
 
+operator fun Vector.plus(v: Vector) = add(v)
+
+operator fun Vector.minus(v: Vector) = subtract(v)
+
 operator fun Vector.times(i: Int) = multiply(i)
 operator fun Vector.times(d: Double) = multiply(d)
 
